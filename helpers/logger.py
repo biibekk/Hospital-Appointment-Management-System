04dad836d39845e0d41f47dd0e17b5ae9acf2011
Bibek <bibek.srestha20@gmail.com>
2026-08-12 22:31:57 +0530

    helper dblogger, try except in schedule service added

---
+++ helpers/logger.py
@@ -0,0 +1,24 @@
+import logging
+
+# logging.basicConfig(format='%(asctime)s %(name)s %(levelname) -8s [%(filename) s:%(lineno)d] %(message)s',
+#                     level=logging.DEBUG,
+#                     filename="logs/Hospital.log",
+#                     datefmt="%d-%m—%Y %H:%M:%S")
+# dblogger = logging.getLogger("Database")
+
+# dblogger.debug("demo")
+
+dbformatter = logging.Formatter(format='%(asctime)s %(name)s %(levelname) -8s [%(filename) s:%(lineno)d %(message)s]')
+
+dblogger = logging.getLogger('Database')
+dblogger.setLevel(logging.DEBUG)
+dblogger.propagate = False
+
+
+db_fh = logging.FileHandler("logs/database.log")
+db_fh.setLevel(logging.DEBUG)
+db_fh.setFormatter(dbformatter)
+
+if not dblogger.handlers:
+    dblogger.addHandler(db_fh)
+
--- services/doctorschedule_service.py
+++ services/doctorschedule_service.py
@@ -1,24 +1,29 @@
+import sqlite3
+from helpers.logger import dblogger
 from models.doctor_model import DoctorModel
-from helpers.display_help import display
+from repositories.doctorschedule_repo import DoctorScheduleRepo   # for function syntax highlight
+from repositories.doctor_repo import DoctorRepo
 
 class DoctorScheduleService:
-    def __init__(self,schedule_obj,doctor_repo):
+    def __init__(self,schedule_obj: DoctorScheduleRepo,doctor_repo: DoctorRepo):
         self.repo = schedule_obj
         self.doctor_repo = doctor_repo
 
     def add_doctor_schedule(self,schedule):
-        doctor = DoctorModel(None,None,None,None,schedule.doctor_id)
-        if not self.doctor_repo.doctor_exists(doctor):
-            display("Doctor Not Found.")
-            return
-        elif self.repo.doctor_schedule_exists(schedule):
-            display("Doctor Schedule Already Exists.")
-            return
+        try:
+            doctor = DoctorModel(None,None,None,None,schedule.doctor_id)
+            if not self.doctor_repo.doctor_exists(doctor):
+                return {'success':False,'message':"Doctor Not Found."}
 
-        res = self.repo.add_doctor_schedule(schedule)
+            if self.repo.doctor_schedule_exists(schedule):
+                return {'success':False,'message':"Doctor Schedule Already Exists."}
 
-        if(res==1):
-            display("Doctor Schedule Added Successfully.")
-        else:
-            display("Doctor Schedule Addition Failed.")
+            res = self.repo.add_doctor_schedule(schedule)
 
+            if(res==1):
+                return {'success':True,'message':"Doctor Schedule Added Successfully."}
+            else:
+                return {'success':False,'message':"Doctor Schedule Addition Failed."}
+        except sqlite3.Error as e:
+            dblogger.error(f"Database error: {e}")
+            return {'success': False,'message': "Unable to add doctor schedule. Please try again."}
